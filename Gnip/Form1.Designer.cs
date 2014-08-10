1859e80624294f3172bfe6c476b44d6ac98384b6
Eduardo Lorenzo <ingeduardojl@gmail.com>
2014-08-09 21:11:33 -0430

    Carga inicial

---
+++ Gnip/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Gnip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerPing = new System.Windows.Forms.Timer(this.components);
+            this.label_ping = new System.Windows.Forms.Label();
+            this.label_bajo = new System.Windows.Forms.Label();
+            this.label_medio = new System.Windows.Forms.Label();
+            this.label_alto = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button_showHideHistory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timerPing
+            // 
+            this.timerPing.Enabled = true;
+            this.timerPing.Interval = 800;
+            this.timerPing.Tick += new System.EventHandler(this.timerPing_Tick);
+            // 
+            // label_ping
+            // 
+            this.label_ping.AutoSize = true;
+            this.label_ping.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ping.Location = new System.Drawing.Point(12, 9);
+            this.label_ping.Name = "label_ping";
+            this.label_ping.Size = new System.Drawing.Size(94, 24);
+            this.label_ping.TabIndex = 0;
+            this.label_ping.Text = "xxxx ms";
+            // 
+            // label_bajo
+            // 
+            this.label_bajo.AutoSize = true;
+            this.label_bajo.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_bajo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label_bajo.Location = new System.Drawing.Point(12, 44);
+            this.label_bajo.Name = "label_bajo";
+            this.label_bajo.Size = new System.Drawing.Size(94, 24);
+            this.label_bajo.TabIndex = 1;
+            this.label_bajo.Text = "xxxx ms";
+            // 
+            // label_medio
+            // 
+            this.label_medio.AutoSize = true;
+            this.label_medio.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_medio.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label_medio.Location = new System.Drawing.Point(12, 68);
+            this.label_medio.Name = "label_medio";
+            this.label_medio.Size = new System.Drawing.Size(94, 24);
+            this.label_medio.TabIndex = 2;
+            this.label_medio.Text = "xxxx ms";
+            // 
+            // label_alto
+            // 
+            this.label_alto.AutoSize = true;
+            this.label_alto.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_alto.ForeColor = System.Drawing.Color.Red;
+            this.label_alto.Location = new System.Drawing.Point(12, 92);
+            this.label_alto.Name = "label_alto";
+            this.label_alto.Size = new System.Drawing.Size(94, 24);
+            this.label_alto.TabIndex = 3;
+            this.label_alto.Text = "xxxx ms";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(122, 9);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(335, 130);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Hora";
+            this.columnHeader1.Width = 148;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tipo";
+            this.columnHeader2.Width = 77;
+            // 
+            // button_showHideHistory
+            // 
+            this.button_showHideHistory.Location = new System.Drawing.Point(70, 119);
+            this.button_showHideHistory.Name = "button_showHideHistory";
+            this.button_showHideHistory.Size = new System.Drawing.Size(34, 20);
+            this.button_showHideHistory.TabIndex = 5;
+            this.button_showHideHistory.Text = "--->";
+            this.button_showHideHistory.UseVisualStyleBackColor = true;
+            this.button_showHideHistory.Click += new System.EventHandler(this.button_showHideHistory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(117, 148);
+            this.Controls.Add(this.button_showHideHistory);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label_alto);
+            this.Controls.Add(this.label_medio);
+            this.Controls.Add(this.label_bajo);
+            this.Controls.Add(this.label_ping);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gnip";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerPing;
+        private System.Windows.Forms.Label label_ping;
+        private System.Windows.Forms.Label label_bajo;
+        private System.Windows.Forms.Label label_medio;
+        private System.Windows.Forms.Label label_alto;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button_showHideHistory;
+    }
+}
+
---
+++ Gnip/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gnip
+{
+    public partial class Form1 : Form
+    {
+        public static int MAX_PINGS_TO_KEEP = 20;
+
+        private List<PingReply> pingReplies = new List<PingReply>();
+
+        // Para el historial
+        private int MAX_HISTORY = 1000;
+        private double SECONDS_BETWEEN_ACTIONS = 7;
+        private double lastTimeAction = 0;
+
+        private void saveHistory(string time, string type)
+        {
+            string fecha = DateTime.Now.Date.Day.ToString() + "-" + DateTime.Now.Date.Month.ToString() + "-" + DateTime.Now.Date.Year.ToString();
+            string ext = ".txt";
+            string filename = fecha + ext;
+
+            string archivoActual = type + " ~ " + time + Environment.NewLine;
+            bool fileReaded = !File.Exists(filename);
+
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(filename);
+                    archivoActual += sr.ReadToEnd();
+                    sr.Close();
+                    fileReaded = true;
+                }
+                catch (Exception e)
+                {
+                }
+
+            }
+
+            if (fileReaded)
+            {
+                try
+                {
+                    System.IO.File.WriteAllText(@filename, archivoActual);
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerPing_Tick(sender, e);            
+        }
+        
+        private void timerPing_Tick(object sender, EventArgs e)
+        {
+            timerPing.Stop();
+
+            Ping ping = new Ping();
+            PingReply pr = null;
+
+            try { pr = ping.Send("www.google.com"); }
+            catch (Exception ex) { }
+
+            bool quit = false;
+
+            if (pr == null) quit = true;
+            if (pr.Status != IPStatus.Success) quit = true;
+
+            if (quit)
+            {
+                timerPing.Start();
+                return;
+            }
+
+
+            label_ping.Text = pr.RoundtripTime.ToString() + " ms";                       
+
+            if (pingReplies.Count == MAX_PINGS_TO_KEEP)
+                pingReplies.RemoveAt(0);
+
+            pingReplies.Add(pr);
+
+
+            EstadisticaPingReply epr = new EstadisticaPingReply(pingReplies);
+            epr.calculate();
+
+            label_bajo.Text = epr.getMin().ToString() + " ms";
+            label_medio.Text = epr.getAvg().ToString() + " ms";
+            label_alto.Text = epr.getMax().ToString() + " ms";
+
+            this.Text = "Gnip";
+            this.Text += " (" + pr.RoundtripTime.ToString() + " ms)";
+
+            if (epr.isDownloading())
+                this.Text += " {Downloading}";
+            else if (epr.isYoutubing())
+                this.Text += " {Youtube}";
+            else if (epr.isBrowsing())
+                this.Text += " {Browsing}";
+            
+
+            // Obtenemos la ultima accion
+            ListViewItem lastAction = epr.getLastAction();
+            // Tiempo en segundos AHORA
+            double now = DateTime.Now.TimeOfDay.TotalSeconds;
+
+            if (lastAction != null && now - lastTimeAction > SECONDS_BETWEEN_ACTIONS)
+            {
+                // Actualizamos el archivo de historial
+                saveHistory(lastAction.SubItems[0].Text, lastAction.SubItems[1].Text);
+
+                // Guardamos el tiempo
+                lastTimeAction = now;
+
+                // Añadimos a la listview
+                listView1.Items.Add(lastAction);
+            }
+
+            // Removemos los history extras
+            if (listView1.Items.Count > MAX_HISTORY)
+                listView1.Items.RemoveAt(0);
+
+            timerPing.Start();
+        }
+
+        private void button_showHideHistory_Click(object sender, EventArgs e)
+        {
+            if (this.Size.Width == 475) // Entonces ocultar
+            {
+                this.Size = new Size(125, this.Size.Height);
+                button_showHideHistory.Text = "--->";
+            }
+            else // Mostrar
+            {
+                this.Size = new Size(475, this.Size.Height);
+                button_showHideHistory.Text = "<---";
+            }
+        }        
+    }
+}
